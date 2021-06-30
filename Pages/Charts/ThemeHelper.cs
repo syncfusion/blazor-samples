3e96c24385999341fbcd8611a8cb99137b8a1b59
aravinth.kumar <aravinth.kumar@syncfusion.com>
2021-06-30 15:57:22 +0530

    v19.2.44 released

---
+++ Pages/Charts/ThemeHelper.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.Blazor;
+
+namespace ThemeHelper
+{
+    internal class ThemeHelper
+    {
+        internal static Theme GetCurrentTheme(string navURL)
+        {
+            if (navURL.IndexOf("material") > -1)
+            {
+                return Theme.Material;
+            }
+            else if (navURL.IndexOf("fabric") > -1)
+            {
+                return Theme.Fabric;
+            }
+            else if (navURL.IndexOf("bootstrap") > -1)
+            {
+                return Theme.Bootstrap;
+            }
+            else if (navURL.IndexOf("highcontrast") > -1)
+            {
+                return Theme.HighContrast;
+            }
+            else if (navURL.IndexOf("tailwind") > -1)
+            {
+                return Theme.Tailwind;
+            }
+            else
+            {
+                return Theme.Bootstrap4;
+            }
+        }
+    }
+}
