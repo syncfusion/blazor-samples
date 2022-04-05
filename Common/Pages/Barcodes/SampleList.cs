78f1ceac45233741cfb08b3c456f6018e0ea2548
SyncfusionBuild <buildautomation@syncfusion.com>
2022-04-05 14:19:20 +0530

    v20.1.0.47

---
+++ Common/Pages/Barcodes/SampleList.cs
@@ -0,0 +1,140 @@
+#region Copyright Syncfusion Inc. 2001-2022.
+// Copyright Syncfusion Inc. 2001-2022. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDemos;
+namespace BlazorDemos
+{
+    internal partial class SampleConfig
+    {
+        public List<Sample> Barcode { get; set; } = new List<Sample>{
+            new Sample
+            {
+                Name = "EAN-8",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/default-functionalities",
+                FileName = "DefaultFunctionalities.razor"
+            },
+            new Sample
+            {
+                Name = "EAN-13",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/ean-13",
+                FileName = "Ean13.razor"
+            },
+            new Sample
+            {
+                Name = "Code 32",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-32",
+                FileName = "Code32.razor"
+            },
+            new Sample
+            {
+                Name = "Code 39",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-39",
+                FileName = "Code39.razor"
+            },
+            new Sample
+            {
+                Name = "Extended Code 39",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/extended-code-39",
+                FileName = "Code39Extension.razor"
+            },
+            new Sample
+            {
+                Name = "Code 93",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-93",
+                FileName = "Code93.razor"
+            },
+            new Sample
+            {
+                Name = "Code 128",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-128",
+                FileName = "Code128.razor"
+            },
+            new Sample
+            {
+                Name = "Code 128A",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-128a",
+                FileName = "Code128A.razor"
+            },
+            new Sample
+            {
+                Name = "Code 128B",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-128b",
+                FileName = "Code128B.razor"
+            },
+            new Sample
+            {
+                Name = "Code 128C",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/code-128c",
+                FileName = "Code128C.razor"
+            },
+            new Sample
+            {
+                Name = "Codabar",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/codabar",
+                FileName = "Codabar.razor"
+            },            
+            new Sample
+            {
+                Name = "UPC-A",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/upc-a",
+                FileName = "UpcA.razor"
+            },
+            new Sample
+            {
+                Name = "UPC-E",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/upc-e",
+                FileName = "UpcE.razor"
+            },
+            new Sample
+            {
+                Name = "QR Code",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/qr-code",
+                FileName = "QRCode.razor"
+            },
+            new Sample
+            {
+                Name = "Data Matrix",
+                Category = "Getting started",
+                Directory = "Barcodes/Barcode",
+                Url = "barcodes/data-matrix",
+                FileName = "DataMatrix.razor"
+            }
+        };
+    }
+}
